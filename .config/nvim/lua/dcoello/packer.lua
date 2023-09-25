b3e4c40145fb8e3a9da6ab9b64617611f3058ab3
David Coello Pulido <davidcoello@eprosima.com>
2023-09-25 21:42:18 +0200

    Add nvim confs

--- .config/nvim/lua/dcoello/packer.lua
+++ .config/nvim/lua/dcoello/packer.lua
@@ -7,13 +7,22 @@ return require('packer').startup(function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
 
+    -- navigation 
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.3',
 		-- or                            , branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
+    use('theprimeagen/harpoon')
 
+    -- theme
     use 'tanvirtin/monokai.nvim'
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+
+    -- languaje support
 	use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 	use {
 		'VonHeikemen/lsp-zero.nvim',
